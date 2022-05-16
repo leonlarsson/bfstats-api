37ef9b9d8354437fe4e8af252f273d18a9c97a43
Leon San José Larsson <leonlarsson8@gmail.com>
2022-05-16 19:42:34 +0200

    first version

--- index.js
+++ index.js
@@ -4,8 +4,47 @@ addEventListener('fetch', event => {
 
 async function handleRequest(request) {
 
+    if (request.method === "POST") {
 
-    return new Response('Hello worker!', {
-        headers: { 'content-type': 'text/plain' },
-    });
+        // IF API-KEY is not correct, return
+        if (request.headers.get("API-KEY") !== API_KEY) {
+            return new Response("Not allowed", {
+                headers: { "Content-Type": "text/plain" },
+                status: 401
+            });
+        }
+
+        const totalGuilds = request.headers.get("TOTAL-GUILDS");
+        const totalUsers = request.headers.get("TOTAL-USERS");
+
+        // If API-KEY is valid
+
+        // If TOTAL-GUILDS header is present, and is an integer, put to KV
+        if (totalGuilds) {
+            if (!Number.parseInt(totalGuilds)) return new Response(`Value ${totalGuilds} for TOTAL-GUILDS is not an integer.`, { headers: { "Content-Type": "text/plain" }, status: 400 });
+            await DATA.put("TOTAL_GUILDS", Number.parseInt(totalGuilds));
+        }
+
+        // If TOTAL-USERS header is present, and is an integer, put to KV
+        if (totalUsers) {
+            if (!Number.parseInt(totalUsers)) return new Response(`Value ${totalUsers} for TOTAL-USERS is not an integer.`, { headers: { "Content-Type": "text/plain" }, status: 400 });
+            await DATA.put("TOTAL_USERS", Number.parseInt(totalUsers));
+        }
+
+        return new Response("Data posted", {
+            headers: { "Content-Type": "text/plain" },
+            status: 200
+        });
+    }
+
+    if (request.method === "GET") {
+
+        const totalGuilds = await DATA.get("TOTAL_GUILDS");
+        const totalUsers = await DATA.get("TOTAL_USERS");
+
+        return new Response(JSON.stringify({ totalGuilds, totalUsers }), {
+            headers: { "Content-Type": "application/json" },
+            status: 200
+        });
+    }
 }
