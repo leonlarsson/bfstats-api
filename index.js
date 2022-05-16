d9ab92261b5a90427aa5dac28dc803ae698dce26
Leon San José Larsson <leonlarsson8@gmail.com>
2022-05-17 00:59:28 +0200

    update lastUpdated response
    Plus minor changes.

--- index.js
+++ index.js
@@ -8,32 +8,34 @@ async function handleRequest(request) {
 
         // IF API-KEY is not correct, return
         if (request.headers.get("API-KEY") !== API_KEY) {
-            return new Response("Not allowed", {
+            return new Response("No valid API key found.", {
                 headers: { "Content-Type": "text/plain" },
                 status: 401
             });
         }
 
+        // TODO: Deal with empty body?
         const { totalGuilds, totalChannels, totalMembers } = await request.json();
 
-        // If a header is present, but not an integer
+        // If a value is present, but not an integer, return error
         if (totalGuilds && !Number.isInteger(Number.parseInt(totalGuilds)) || totalChannels && !Number.isInteger(Number.parseInt(totalChannels)) && totalMembers && !Number.isInteger(Number.parseInt(totalMembers))) {
             return new Response(`Value ${totalGuilds} (totalGuilds) or ${totalChannels} (totalChannels) or ${totalMembers} (totalChannels) is not an integer.`, { headers: { "Content-Type": "text/plain" }, status: 400 });
         }
 
-        // If TOTAL-GUILDS header is present, and is an integer, put to KV
+        // If totalGuilds is present, and is an integer, put to KV
         if (totalGuilds) await DATA.put("TOTAL_GUILDS", Number.parseInt(totalGuilds));
 
-        // If TOTAL-CHANNELS header is present, and is an integer, put to KV
+        // If totalChannels is present, and is an integer, put to KV
         if (totalChannels) await DATA.put("TOTAL_CHANNELS", Number.parseInt(totalChannels));
 
-        // If TOTAL-MEMBERS header is present, and is an integer, put to KV
+        // If totalMembers is present, and is an integer, put to KV
         if (totalMembers) await DATA.put("TOTAL_MEMBERS", Number.parseInt(totalMembers));
 
         // Add last updated KV
-        await DATA.put("LAST_UPDATED", new Date().toUTCString());
+        const date = new Date();
+        await DATA.put("LAST_UPDATED", JSON.stringify({ date: new Date().toUTCString(), timestampMiliseconds: date.valueOf(), timestampSeconds: Math.floor(date.valueOf() / 1000) }));
 
-        return new Response("Data posted", {
+        return new Response("Data posted.", {
             headers: { "Content-Type": "text/plain" },
             status: 200
         });
@@ -41,12 +43,12 @@ async function handleRequest(request) {
 
     if (request.method === "GET") {
 
-        const lastUpdated = await DATA.get("LAST_UPDATED");
+        const { date, timestampMilliseconds, timestampSeconds } = JSON.parse(await DATA.get("LAST_UPDATED"));
         const totalGuilds = Number.parseInt(await DATA.get("TOTAL_GUILDS"));
         const totalChannels = Number.parseInt(await DATA.get("TOTAL_CHANNELS"));
         const totalMembers = Number.parseInt(await DATA.get("TOTAL_MEMBERS"));
 
-        return new Response(JSON.stringify({ lastUpdated, totalGuilds, totalChannels, totalMembers }), {
+        return new Response(JSON.stringify({ lastUpdated: { date, timestampMilliseconds, timestampSeconds }, totalGuilds, totalChannels, totalMembers }), {
             headers: { "Content-Type": "application/json" },
             status: 200
         });
