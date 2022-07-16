1b3fbe36a77d0c1c9d6972e4e643e335f7c5f8d8
Leon San José Larsson <leonlarsson8@gmail.com>
2022-07-16 17:37:23 +0200

    validate statsObject from KV

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { ZodError } from "zod";
-import { ReceivedBodySchema, ReceivedBody, StatsObject } from "./types";
+import { ReceivedBody, ReceivedBodySchema, StatsObject, StatsObjectSchema } from "./types";
 
 const KV_STATS = '{"totalGuilds":1686,"totalChannels":57043,"totalMembers":539451,"totalStatsSent":{"total":101516,"games":{"Battlefield 2042":8280,"Battlefield V":43686,"Battlefield 1":18421,"Battlefield Hardline":1331,"Battlefield 4":22091,"Battlefield 3":2850,"Battlefield Bad Company 2":148,"Battlefield 2":147},"languages":{"English":60657,"French":924,"Italian":279,"German":748,"Spanish":622,"Russian":973,"Polish":473,"Brazilian Portuguese":1254,"Turkish":471,"Swedish":298,"Norwegian":26,"Finnish":95,"Arabic":106}},"lastUpdated":{"date":"Sat, 16 Jul 2022 13:36:17 GMT","timestampMilliseconds":1657978577513,"timestampSeconds":1657978577}}';
 
@@ -35,20 +35,23 @@ export default {
 
             // Get the KV STATS object and edit it accordingly, before .put()ing it back
             const statsObject: StatsObject = await env.DATA.get("STATS", { type: "json" });
+            try {
+                StatsObjectSchema.parse(statsObject);
+            } catch (err) {
+                return new Response("KV statsObject doesn't match schema. Not updating KV. Received the following ZodError: " + JSON.stringify(err.format()), {
+                    headers: { "Content-Type": "application/json" },
+                    status: 500
+                });
+            }
             statsObject.totalGuilds = totalGuilds;
             statsObject.totalChannels = totalChannels;
             statsObject.totalMembers = totalMembers;
 
             if (incrementTotalStatsSent === true) {
-
-                // Get total and the specific game to increment
+                // Increment total and specific game and language. Only increment game/language if defined
                 statsObject.totalStatsSent.total++;
-                const totalStatsSentGame = game ? statsObject.totalStatsSent.games[game] : null;
-                const totalStatsSentLanguage = language ? statsObject.totalStatsSent.languages[language] : null;
-
-                // Increment specific game and language, if defined and an is an integer
-                if (game && Number.isInteger(totalStatsSentGame)) statsObject.totalStatsSent.games[game]++;
-                if (language && Number.isInteger(totalStatsSentLanguage)) statsObject.totalStatsSent.languages[language]++;
+                if (game) statsObject.totalStatsSent.games[game]++;
+                if (language) statsObject.totalStatsSent.languages[language]++;
             }
 
             // Add lastUpdated to the object
