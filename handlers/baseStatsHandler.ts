b86295abcc0385e71466aceb80032601f10acd74
Leon San José Larsson <leonlarsson8@gmail.com>
2023-05-18 21:44:34 +0200

    update KV backup

--- handlers/baseStatsHandler.ts
+++ handlers/baseStatsHandler.ts
@@ -1,7 +1,7 @@
 import { ZodError } from "zod";
 import { Environment, BaseReceivedBody, BaseReceivedBodySchema, BaseStatsObject, BaseStatsObjectSchema } from "../types";
 
-const KV_STATS = '{"totalGuilds":2147,"totalChannels":70328,"totalMembers":535048,"totalStatsSent":{"total":126573,"games":{"Battlefield 2042":16983,"Battlefield V":51383,"Battlefield 1":22220,"Battlefield Hardline":1477,"Battlefield 4":26170,"Battlefield 3":3409,"Battlefield Bad Company 2":195,"Battlefield 2":166},"languages":{"English":80330,"French":1883,"Italian":459,"German":1566,"Spanish":982,"Russian":1767,"Polish":1110,"Brazilian Portuguese":2191,"Turkish":907,"Swedish":410,"Norwegian":99,"Finnish":149,"Arabic":112}},"lastUpdated":{"date":"Sat, 03 Dec 2022 15:03:09 GMT","timestampMilliseconds":1670079789230,"timestampSeconds":1670079789}}';
+const KV_STATS = '{"totalGuilds":2904,"totalChannels":88077,"totalMembers":594252,"totalStatsSent":{"total":157170,"games":{"Battlefield 2042":30604,"Battlefield V":58745,"Battlefield 1":26474,"Battlefield Hardline":1663,"Battlefield 4":30300,"Battlefield 3":4309,"Battlefield Bad Company 2":296,"Battlefield 2":209},"languages":{"English":101503,"French":3510,"Italian":682,"German":3038,"Spanish":1946,"Russian":3275,"Polish":2312,"Brazilian Portuguese":3717,"Turkish":1441,"Swedish":544,"Norwegian":139,"Finnish":315,"Arabic":140}},"lastUpdated":{"date":"Thu, 18 May 2023 19:41:12 GMT","timestampMilliseconds":1684438872486,"timestampSeconds":1684438872}}';
 
 export default async (request: Request, env: Environment): Promise<Response> => {
 
