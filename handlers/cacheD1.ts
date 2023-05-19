e06fdd8ca7c134f591d7c0fd406a47fae5e727f5
Leon San José Larsson <leonlarsson8@gmail.com>
2023-05-19 22:16:11 +0200

    cacheD1: log error no longer

--- handlers/cacheD1.ts
+++ handlers/cacheD1.ts
@@ -1,13 +1,9 @@
 import { Environment } from "../types";
 
 export default async (env: Environment) => {
-    try {
-        const { results: outputs } = await env.DB.prepare("SELECT game, segment, language, date FROM outputs").all();
-        await env.DATA_KV.put("D1_OUTPUTS", JSON.stringify({ cached: new Date().getTime(), outputs }));
-        
-        const { results: users } = await env.DB.prepare("SELECT total_stats_sent FROM users ORDER BY total_stats_sent DESC").all();
-        await env.DATA_KV.put("D1_USERS", JSON.stringify({ cached: new Date().getTime(), users }));
-    } catch (error) {
-        console.log(error.cause);
-    }
+  const { results: outputs } = await env.DB.prepare("SELECT game, segment, language, date FROM outputs").all();
+  await env.DATA_KV.put("D1_OUTPUTS", JSON.stringify({ cached: new Date().getTime(), outputs }));
+
+  const { results: users } = await env.DB.prepare("SELECT total_stats_sent FROM users ORDER BY total_stats_sent DESC").all();
+  await env.DATA_KV.put("D1_USERS", JSON.stringify({ cached: new Date().getTime(), users }));
 };
