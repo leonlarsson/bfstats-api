adb45369739324e505c3fc9db56c2dcde23dfe3f
Leon San José Larsson <leonlarsson8@gmail.com>
2023-05-20 13:49:14 +0200

    formatting

--- handlers/D1Handler.ts
+++ handlers/D1Handler.ts
@@ -55,44 +55,44 @@ export default async (request: Request, env: Environment): Promise<Response> => 
     if (request.method === "POST") {
         // D1 users
         if (url.pathname === "/d1/users") {
-                if (!isAdmin) return _401();
-    
-                try {
-    
-                    const { userId, username, language }: D1UserPayload = await request.json();
-    
-                    // Insert a new user. If there is a conflict (user_id already exists), then update the existing row
-                    await env.DB.prepare(`
+            if (!isAdmin) return _401();
+
+            try {
+
+                const { userId, username, language }: D1UserPayload = await request.json();
+
+                // Insert a new user. If there is a conflict (user_id already exists), then update the existing row
+                await env.DB.prepare(`
                         INSERT INTO users (user_id, username, last_stats_sent, last_language, total_stats_sent) 
                         VALUES (?1, ?2, ?3, ?4, 1) 
                         ON CONFLICT (user_id) DO UPDATE SET username = ?2, last_stats_sent = ?3, last_language = ?4, total_stats_sent = total_stats_sent + 1 
                         WHERE user_id = ?1
                         `).bind(userId, username, new Date().getTime(), language).run();
-    
-                    return new Response("POST /d1/users OK");
-    
-                } catch (e) {
-                    console.log({ message: e.message, cause: e.cause?.message });
-                    return json({ message: e.message, cause: e.cause?.message }, 500);
-                }
+
+                return new Response("POST /d1/users OK");
+
+            } catch (e) {
+                console.log({ message: e.message, cause: e.cause?.message });
+                return json({ message: e.message, cause: e.cause?.message }, 500);
+            }
         }
-    
+
         // D1 outputs
         if (url.pathname === "/d1/outputs") {
-                if (!isAdmin) return _401();
-    
-                try {
-    
-                    const { userId, username, guildName, guildId, game, segment, language, messageURL, imageURL }: D1OutputPayload = await request.json();
-    
-                    await env.DB.prepare(`INSERT INTO outputs (user_id, username, guild_name, guild_id, game, segment, language, date, message_url, image_url) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10)`).bind(userId, username, guildName, guildId, game, segment, language, new Date().getTime(), messageURL, imageURL).run();
-    
-                    return new Response("POST /d1/outputs OK");
-    
-                } catch (e) {
-                    console.log({ message: e.message, cause: e.cause?.message });
-                    return json({ message: e.message, cause: e.cause?.message }, 500);
-                }
+            if (!isAdmin) return _401();
+
+            try {
+
+                const { userId, username, guildName, guildId, game, segment, language, messageURL, imageURL }: D1OutputPayload = await request.json();
+
+                await env.DB.prepare(`INSERT INTO outputs (user_id, username, guild_name, guild_id, game, segment, language, date, message_url, image_url) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10)`).bind(userId, username, guildName, guildId, game, segment, language, new Date().getTime(), messageURL, imageURL).run();
+
+                return new Response("POST /d1/outputs OK");
+
+            } catch (e) {
+                console.log({ message: e.message, cause: e.cause?.message });
+                return json({ message: e.message, cause: e.cause?.message }, 500);
+            }
         }
     }
 }
--- handlers/sendEmail.ts
+++ handlers/sendEmail.ts
@@ -1,38 +1,34 @@
 import { BaseStatsObject, Environment } from "../types";
 
 export default async (env: Environment) => {
-  const statsObject: BaseStatsObject = await env.DATA_KV.get("STATS", { type: "json" });
+    const statsObject: BaseStatsObject = await env.DATA_KV.get("STATS", { type: "json" });
 
-  return fetch("https://api.resend.com/emails", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${env.RESEND_API_KEY}`
-    },
-    body: JSON.stringify({
-      from: "Battlefield Stats Worker <bfstats@leonlarsson.com>",
-      to: "leonlarsson8@gmail.com",
-      subject: "Battlefield Stats Update",
-      html: `<h2>Base:</h2>
-      <ul>
-      <li>Total Guilds: <b>${statsObject.totalGuilds.toLocaleString("en-US")}</b></li>
-      <li>Total Channels: <b>${statsObject.totalChannels.toLocaleString("en-US")}</b></li>
-      <li>Total Members: <b>${statsObject.totalMembers.toLocaleString("en-US")}</b></li>
-      </ul>
-      <h2>Stats Sent:</h2>
-      Total: <b>${statsObject.totalStatsSent.total.toLocaleString("en-US")}</b>
-      <h3>Games</h3>
-      <ul>
-      ${Object.entries(statsObject.totalStatsSent.games)
-        .map(x => `<li>${x[0]}: <b>${x[1].toLocaleString("en-US")}</b></li>`)
-        .join("\n")}
-      </ul>
-      <h3>Languages</h3>
-      <ul>
-      ${Object.entries(statsObject.totalStatsSent.languages)
-        .map(x => `<li>${x[0]}: <b>${x[1].toLocaleString("en-US")}</b></li>`)
-        .join("\n")}
-      </ul>`
-    })
-  });
+    return fetch("https://api.resend.com/emails", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${env.RESEND_API_KEY}`
+        },
+        body: JSON.stringify({
+            from: "Battlefield Stats Worker <bfstats@leonlarsson.com>",
+            to: "leonlarsson8@gmail.com",
+            subject: "Battlefield Stats Update",
+            html: `<h2>Base:</h2>
+            <ul>
+            <li>Total Guilds: <b>${statsObject.totalGuilds.toLocaleString("en-US")}</b></li>
+            <li>Total Channels: <b>${statsObject.totalChannels.toLocaleString("en-US")}</b></li>
+            <li>Total Members: <b>${statsObject.totalMembers.toLocaleString("en-US")}</b></li>
+            </ul>
+            <h2>Stats Sent:</h2>
+            Total: <b>${statsObject.totalStatsSent.total.toLocaleString("en-US")}</b>
+            <h3>Games</h3>
+            <ul>
+            ${Object.entries(statsObject.totalStatsSent.games).map(x => `<li>${x[0]}: <b>${x[1].toLocaleString("en-US")}</b></li>`).join("\n")}
+            </ul>
+            <h3>Languages</h3>
+            <ul>
+            ${Object.entries(statsObject.totalStatsSent.languages).map(x => `<li>${x[0]}: <b>${x[1].toLocaleString("en-US")}</b></li>`).join("\n")}
+            </ul>`
+        })
+    });
 };
