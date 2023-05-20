88993d06ef429aece8b70f599e3ecf8449c9fed5
Leon San José Larsson <leonlarsson8@gmail.com>
2023-05-20 13:29:03 +0200

    D1: remove massive GETs

--- handlers/D1Handler.ts
+++ handlers/D1Handler.ts
@@ -21,37 +21,6 @@ export default async (request: Request, env: Environment): Promise<Response> => 
         }
     }
 
-    // SEMI-PUBLIC - D1 users
-    if (url.pathname === "/d1/users") {
-        if (request.method === "POST") {
-
-            if (!isAdmin) return _401();
-
-            try {
-
-                const { userId, username, language }: D1UserPayload = await request.json();
-
-                // Insert a new user. If there is a conflict (user_id already exists), then update the existing row
-                await env.DB.prepare(`
-                    INSERT INTO users (user_id, username, last_stats_sent, last_language, total_stats_sent) 
-                    VALUES (?1, ?2, ?3, ?4, 1) 
-                    ON CONFLICT (user_id) DO UPDATE SET username = ?2, last_stats_sent = ?3, last_language = ?4, total_stats_sent = total_stats_sent + 1 
-                    WHERE user_id = ?1
-                    `).bind(userId, username, new Date().getTime(), language).run();
-
-                return new Response("POST /d1/users OK");
-
-            } catch (e) {
-                console.log({ message: e.message, cause: e.cause?.message });
-                return json({ message: e.message, cause: e.cause?.message }, 500);
-            }
-
-        } else if (request.method === "GET") {
-            const { results } = await env.DB.prepare(isAdmin ? "SELECT * FROM users" : "SELECT total_stats_sent FROM users ORDER BY total_stats_sent DESC").all();
-            return json(results);
-        }
-    }
-
     // PUBLIC - D1 users (top 20)
     if (request.method === "GET" && url.pathname === "/d1/users/limited") {
         const { results } = await env.DB.prepare("SELECT total_stats_sent FROM users ORDER BY total_stats_sent DESC LIMIT 20").all();
@@ -70,31 +39,6 @@ export default async (request: Request, env: Environment): Promise<Response> => 
         return json(results);
     }
 
-    // SEMI-PUBLIC D1 outputs
-    if (url.pathname === "/d1/outputs") {
-        if (request.method === "POST") {
-
-            if (!isAdmin) return _401();
-
-            try {
-
-                const { userId, username, guildName, guildId, game, segment, language, messageURL, imageURL }: D1OutputPayload = await request.json();
-
-                await env.DB.prepare(`INSERT INTO outputs (user_id, username, guild_name, guild_id, game, segment, language, date, message_url, image_url) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10)`).bind(userId, username, guildName, guildId, game, segment, language, new Date().getTime(), messageURL, imageURL).run();
-
-                return new Response("POST /d1/outputs OK");
-
-            } catch (e) {
-                console.log({ message: e.message, cause: e.cause?.message });
-                return json({ message: e.message, cause: e.cause?.message }, 500);
-            }
-
-        } else if (request.method === "GET") {
-            const { results } = await env.DB.prepare(isAdmin ? "SELECT * FROM outputs" : "SELECT game, segment, language, date FROM outputs").all();
-            return json(results);
-        }
-    }
-
     // PUBLIC - D1 outputs (last 20)
     if (request.method === "GET" && url.pathname === "/d1/outputs/limited") {
         const { results } = await env.DB.prepare("SELECT game, segment, language, date FROM outputs ORDER BY date DESC LIMIT 20").all();
@@ -105,5 +49,50 @@ export default async (request: Request, env: Environment): Promise<Response> => 
     if (request.method === "GET" && url.pathname === "/d1/outputs/counts") {
         const { results } = await env.DB.prepare("SELECT 'game' as category, game as item, COUNT(*) as sent FROM outputs GROUP BY game UNION ALL SELECT 'segment' as category, segment as item, COUNT(*) as sent FROM outputs GROUP BY segment UNION ALL SELECT 'language' as category, language as item, COUNT(*) as sent FROM outputs GROUP BY language ORDER BY category ASC, sent DESC").all();
         return json(results);
+    }
+
+    // POSTS
+    if (request.method === "POST") {
+        // D1 users
+        if (url.pathname === "/d1/users") {
+                if (!isAdmin) return _401();
+    
+                try {
+    
+                    const { userId, username, language }: D1UserPayload = await request.json();
+    
+                    // Insert a new user. If there is a conflict (user_id already exists), then update the existing row
+                    await env.DB.prepare(`
+                        INSERT INTO users (user_id, username, last_stats_sent, last_language, total_stats_sent) 
+                        VALUES (?1, ?2, ?3, ?4, 1) 
+                        ON CONFLICT (user_id) DO UPDATE SET username = ?2, last_stats_sent = ?3, last_language = ?4, total_stats_sent = total_stats_sent + 1 
+                        WHERE user_id = ?1
+                        `).bind(userId, username, new Date().getTime(), language).run();
+    
+                    return new Response("POST /d1/users OK");
+    
+                } catch (e) {
+                    console.log({ message: e.message, cause: e.cause?.message });
+                    return json({ message: e.message, cause: e.cause?.message }, 500);
+                }
+        }
+    
+        // D1 outputs
+        if (url.pathname === "/d1/outputs") {
+                if (!isAdmin) return _401();
+    
+                try {
+    
+                    const { userId, username, guildName, guildId, game, segment, language, messageURL, imageURL }: D1OutputPayload = await request.json();
+    
+                    await env.DB.prepare(`INSERT INTO outputs (user_id, username, guild_name, guild_id, game, segment, language, date, message_url, image_url) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10)`).bind(userId, username, guildName, guildId, game, segment, language, new Date().getTime(), messageURL, imageURL).run();
+    
+                    return new Response("POST /d1/outputs OK");
+    
+                } catch (e) {
+                    console.log({ message: e.message, cause: e.cause?.message });
+                    return json({ message: e.message, cause: e.cause?.message }, 500);
+                }
+        }
     }
 }
