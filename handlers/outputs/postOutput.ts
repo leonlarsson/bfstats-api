cb504a0d3c40a5d62ce343d2ea46a506011d98ad
Leon San José Larsson <leonlarsson8@gmail.com>
2023-07-25 20:52:52 +0200

    use zod for all POSTs

--- handlers/outputs/postOutput.ts
+++ handlers/outputs/postOutput.ts
@@ -1,11 +1,16 @@
 import { Context } from "hono";
-import { Bindings, D1OutputPayload } from "../../types";
+import { Bindings, D1OutputPayload, D1OutputPayloadSchema } from "../../types";
 import handleAndLogD1Error from "../../utils/handleAndLogD1Error";
 
 export default async (c: Context<{ Bindings: Bindings }>) => {
-  try {
-    const { userId, username, guildName, guildId, game, segment, language, messageURL, imageURL }: D1OutputPayload = await c.req.json();
+  const body: D1OutputPayload = await c.req.json().catch(() => ({}));
 
+  // Verify the request body matches the schema
+  const bodyZodReturn = D1OutputPayloadSchema.safeParse(body);
+  if (bodyZodReturn.success === false) return c.json({ message: "Request body did not match schema.", error: bodyZodReturn.error }, 400);
+  const { userId, username, guildName, guildId, game, segment, language, messageURL, imageURL } = body;
+
+  try {
     await c.env.DB.prepare(`INSERT INTO outputs (user_id, username, guild_name, guild_id, game, segment, language, date, message_url, image_url) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10)`)
       .bind(userId, username, guildName, guildId, game, segment, language, new Date().getTime(), messageURL, imageURL)
       .run();
--- handlers/users/postUser.ts
+++ handlers/users/postUser.ts
@@ -1,11 +1,16 @@
 import { Context } from "hono";
-import { Bindings, D1UserPayload } from "../../types";
+import { Bindings, D1UserPayload, D1UserPayloadSchema } from "../../types";
 import handleAndLogD1Error from "../../utils/handleAndLogD1Error";
 
 export default async (c: Context<{ Bindings: Bindings }>) => {
-  try {
-    const { userId, username, language }: D1UserPayload = await c.req.json();
+  const body: D1UserPayload = await c.req.json().catch(() => ({}));
 
+  // Verify the request body matches the schema
+  const bodyZodReturn = D1UserPayloadSchema.safeParse(body);
+  if (bodyZodReturn.success === false) return c.json({ message: "Request body did not match schema.", error: bodyZodReturn.error }, 400);
+  const { userId, username, language } = body;
+
+  try {
     // Insert a new user. If there is a conflict (user_id already exists), then update the existing row
     await c.env.DB.prepare("INSERT INTO users (user_id, username, last_stats_sent, last_language, total_stats_sent) VALUES (?1, ?2, ?3, ?4, 1) ON CONFLICT (user_id) DO UPDATE SET username = ?2, last_stats_sent = ?3, last_language = ?4, total_stats_sent = total_stats_sent + 1 WHERE user_id = ?1")
       .bind(userId, username, new Date().getTime(), language)
