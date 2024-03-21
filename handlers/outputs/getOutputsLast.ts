2ee9ad7c3c61a2353d8477375d09df228c52e7c0
Leon San José Larsson <leonlarsson8@gmail.com>
2024-03-21 11:57:04 +0100

    /outputs/last: return identifier

--- handlers/outputs/getOutputsLast.ts
+++ handlers/outputs/getOutputsLast.ts
@@ -4,7 +4,7 @@ import handleAndLogD1Error from "../../utils/handleAndLogD1Error";
 
 export default async (c: Context<{ Bindings: Bindings }>) => {
   try {
-    const { results } = await c.env.DB.prepare("SELECT game, segment, language, date FROM outputs ORDER BY date DESC LIMIT 20").all();
+    const { results } = await c.env.DB.prepare("SELECT game, segment, language, date, identifier FROM outputs ORDER BY date DESC LIMIT 20").all();
     return c.json(results);
   } catch (error) {
     return handleAndLogD1Error(error);
