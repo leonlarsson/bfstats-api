2c1a7feb1a7c11b07f454957499b8410a7aee1ba
Leon San José Larsson <leonlarsson8@gmail.com>
2024-03-22 11:39:36 +0100

    /outputs/:id: add possible auth

--- handlers/outputs/getOutputByIdentifier.ts
+++ handlers/outputs/getOutputByIdentifier.ts
@@ -2,13 +2,14 @@ import { Context } from "hono";
 import { Bindings } from "../../types";
 import handleAndLogD1Error from "../../utils/handleAndLogD1Error";
 
-type PartialOutput = { game: string; segment: string; language: string; date: string; identifier: string };
-
 export default async (c: Context<{ Bindings: Bindings }>) => {
   const { id } = c.req.param();
+  const authorized = c.get("authorized");
 
   try {
-    const result = await c.env.DB.prepare("SELECT game, segment, language, date, identifier FROM outputs WHERE identifier = ?1 OR identifier LIKE '%' || ?1 || '%' LIMIT 1").bind(id).first<PartialOutput>();
+    const result = await c.env.DB.prepare(`SELECT ${authorized ? "*" : "game, segment, language, date, identifier"} FROM outputs WHERE identifier = ?1 OR identifier LIKE '%' || ?1 || '%' LIMIT 1`)
+      .bind(id)
+      .first();
     return result ? c.json(result) : c.json({ error: "Found no matching identifier" }, 404);
   } catch (error) {
     return handleAndLogD1Error(error);
