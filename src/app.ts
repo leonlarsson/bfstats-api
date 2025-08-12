ae2f7d2e07a746d8527709f6b4cba9f41bd8a2b4
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2025-08-12 21:21:18 +0200

    code herd

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,10 @@ import { events } from "@/db/schema";
 import { configureOpenAPI } from "@/lib/configure-open-api";
 import { createApp } from "@/lib/create-app";
 import { baseRouter } from "@/routes/base/base.index";
+import { handleDiscordWebhooks } from "@/routes/discord-webhooks";
 import { eventsRouter } from "@/routes/events/events.index";
 import { outputsRouter } from "@/routes/outputs/outputs.index";
 import { usersRouter } from "@/routes/users/users.index";
-import { AppEvent } from "@/utils/constants";
-import { sendDiscordWebhook, validateSecurityHeaders } from "@/utils/discordApiUtils";
-import {
-  type APIWebhookEvent,
-  ApplicationIntegrationType,
-  ApplicationWebhookEventType,
-  ApplicationWebhookType,
-} from "discord-api-types/v10";
 
 const app = createApp();
 
@@ -26,67 +19,7 @@ for (const route of routes) {
 }
 
 // Discord webhooks
-app.post("/discord-webhooks", async (c) => {
-  const verified = await validateSecurityHeaders(c.req.raw);
-  if (!verified) {
-    return new Response("invalid request signature", { status: 401 });
-  }
-
-  const webhook = await c.req.json<APIWebhookEvent>();
-
-  if (webhook.type === ApplicationWebhookType.Ping) {
-    return new Response(null, { status: 204 });
-  }
-
-  // ApplicationAuthorized includes both when a user installs the app and when a bot is added to a guild
-  if (webhook.event.type === ApplicationWebhookEventType.ApplicationAuthorized) {
-    const installedToUser = webhook.event.data.integration_type === ApplicationIntegrationType.UserInstall;
-    const { user } = webhook.event.data;
-
-    if (installedToUser) {
-      c.executionCtx.waitUntil(
-        sendDiscordWebhook(
-          c.env.DISCORD_JOINS_WEBHOOK_URL,
-          `:person_bald: - Bot was installed to account **${user.username}** (${user.global_name}) (<@${user.id}>)`,
-        ),
-      );
-    }
-
-    try {
-      await c
-        .get("db")
-        .insert(events)
-        .values({
-          event: installedToUser ? AppEvent.AppUserInstall : AppEvent.AppGuildInstall,
-        });
-    } catch (error: any) {
-      console.error("Error inserting ApplicationAuthorized event:", error);
-    }
-  }
-
-  // ApplicationDeauthorized does NOT include when a bot is simply kicked OR de-authed from a guild
-  // It only is called when a user de-auths it from their own account
-  if (webhook.event.type === ApplicationWebhookEventType.ApplicationDeauthorized) {
-    const { user } = webhook.event.data;
-
-    c.executionCtx.waitUntil(
-      sendDiscordWebhook(
-        c.env.DISCORD_JOINS_WEBHOOK_URL,
-        `:no_entry_sign: - Bot was deauthorized by user **${user.username}** (${user.global_name}) (<@${user.id}>)`,
-      ),
-    );
-
-    try {
-      await c.get("db").insert(events).values({
-        event: AppEvent.AppUserUninstall,
-      });
-    } catch (error: any) {
-      console.error("Error inserting ApplicationDeauthorized event:", error);
-    }
-  }
-
-  return new Response("ok");
-});
+app.post("/discord-webhooks", handleDiscordWebhooks);
 
 export type AppType = (typeof routes)[number];
 
